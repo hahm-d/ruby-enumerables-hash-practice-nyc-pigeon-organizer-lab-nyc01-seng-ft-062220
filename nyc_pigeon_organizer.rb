1901d43b2c95165c9e0f226dcfab616d850371f0
Daniel Hahm <dhahm01@gmail.com>
2020-06-12 01:47:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,15 +3,17 @@ def nyc_pigeon_organizer(data)
   result = data.each_with_object({}) do |(key, value), final_output|
     value.each do |key2, value2|
       value2.each do |name|
-        if !final_output[name]
-          final_output[name] = {}
-        end
-        if !final_output[name][key]
-          final_output[name][key] = []
-        end
-        final_output[name][key].push(key2.to_s)
+    if !final[key]
+      final[key] = {}
+    end
+    value.each do |emo|
+      if !final[key][:english]
+        final[key][:english] = emo
+      end
+      if !final[key][:japanese]
+        final[key][:japanese] = emo
       end
     end
-  end 
-  result
+  end
+  output
 end
